3111ae9793ff39bab811df16c91617c8c2be282d
franco miranda <franco.miranda.2107@gmail.com>
2024-06-29 18:56:40 -0400

    django

--- megagames/models.py
+++ megagames/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class juego(models.Model):
     id=models.IntegerField(primary_key=True)
     nombre=models.CharField(max_length=100)
-    precio=models.DecimalField(max_digits=6, decimal_places=2)
+    precio=models.IntegerField()
     imagen=models.ImageField(upload_to='juegos/',null=True, blank=True)
     descripcion=models.TextField()
     stock=models.IntegerField(default=0)
--- megagames/views.py
+++ megagames/views.py
@@ -1,28 +1,39 @@
 from django.shortcuts import render
+from .models import juego
+
 def principal (request):
     context ={}
     return render (request, 'megagames/principal.html',context)
+
 def soporte (request):
     context ={}
     return render (request, 'megagames/soporte.html',context)
+
 def acercade (request):
     context ={}
     return render (request, 'megagames/acerca-de.html',context)
+
 def carrito (request):
     context ={}
     return render (request, 'megagames/carrito.html',context)
+
 def crearusuario (request):
     context ={}
     return render (request, 'megagames/crear-usuario.html',context)
+
 def juegounico (request):
     context ={}
     return render (request, 'megagames/juego-unico.html',context)
-def juegos (request):
-    context ={}
-    return render (request, 'megagames/juegos.html',context)
+
+def juegos(request):
+    juegos = juego.objects.all()
+    context = {'juegos': juegos}
+    return render(request, 'megagames/juegos.html', context)
+
 def login (request):
     context ={}
     return render (request, 'megagames/login.html',context)
+
 def tienda (request):
     context ={}
     return render (request, 'megagames/tienda.html',context)
